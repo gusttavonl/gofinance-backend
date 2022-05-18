104e90660210c19f2a47944c4372bc133160d9f7
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-05-18 08:00:35 -0300

    feat: add account query to crud

---
+++ db/query/account.sql
@@ -0,0 +1,51 @@
+-- name: CreateAccount :one
+INSERT INTO accounts (
+  user_id,
+  category_id,
+  title,
+  type,
+  description,
+  value,
+  date
+) VALUES (
+  $1, $2, $3, $4, $5, $6, $7
+) RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: GetAccounts :many
+SELECT 
+  a.id,
+  a.user_id,
+  a.title,
+  a.type,
+  a.description,
+  a.value,
+  a.date,
+  a.created_at,
+  c.title as category_title
+FROM accounts a
+LEFT JOIN categories c ON c.id = a.category_id 
+where a.user_id = $1 and a.type = $2 
+and a.category_id = $3 and a.title like $4
+and a.description like $5 and a.date = $6;
+
+-- name: GetAccountsReports :one
+SELECT SUM(value) AS sum_value FROM accounts 
+where user_id = $1 and type = $2;
+
+-- name: GetAccountsGraph :one
+SELECT COUNT(*) FROM accounts 
+where user_id = $1 and type = $2;
+
+-- name: UpdateAccount :one
+UPDATE accounts
+SET title = $2, description = $3, value = $4
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE id = $1;
