f2afd352d6c29e122a439116ea4cf15888519545
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-05-18 08:01:48 -0300

    feat: add category query to crud

---
+++ db/query/category.sql
@@ -0,0 +1,28 @@
+-- name: CreateCategory :one
+INSERT INTO categories (
+  user_id,
+  title,
+  type,
+  description
+) VALUES (
+  $1, $2, $3, $4
+) RETURNING *;
+
+-- name: GetCategory :one
+SELECT * FROM categories
+WHERE id = $1 LIMIT 1;
+
+-- name: GetCategories :many
+SELECT * FROM categories 
+where user_id = $1 and type = $2 
+and title like $3 and description like $4;
+
+-- name: UpdateCategories :one
+UPDATE categories
+SET title = $2, description = $3
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteCategories :exec
+DELETE FROM categories
+WHERE id = $1;
