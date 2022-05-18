14aa8d80fd58cd3339d09853a14d68e8cf89a962
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-05-18 08:04:51 -0300

    feat: add querier and store

---
+++ db/sqlc/store.go
@@ -0,0 +1,19 @@
+package db
+
+import "database/sql"
+
+type Store interface {
+	Querier
+}
+
+type SQLStore struct {
+	db *sql.DB
+	*Queries
+}
+
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
+		db:      db,
+		Queries: New(db),
+	}
+}
