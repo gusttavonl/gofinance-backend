eeb62f7251f800cb07e4204cd86dd8cbb9bc2874
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-05-18 08:05:34 -0300

    test: add test to user

---
+++ db/sqlc/user_test.go
@@ -0,0 +1,56 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/GustavoNoronha0/go-finance/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomUser(t *testing.T) User {
+	arg := CreateUserParams{
+		Username: util.RandomString(6),
+		Password: util.RandomString(12),
+		Email:    util.RandomEmail(8),
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.Username, user.Username)
+	require.Equal(t, arg.Password, user.Password)
+	require.Equal(t, arg.Email, user.Email)
+	require.NotEmpty(t, user.CreatedAt)
+
+	return user
+}
+
+func TestCreateUser(t *testing.T) {
+	createRandomUser(t)
+}
+
+func TestGetUser(t *testing.T) {
+	user1 := createRandomUser(t)
+	user2, err := testQueries.GetUser(context.Background(), user1.Username)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user1.Username, user2.Username)
+	require.Equal(t, user1.Password, user2.Password)
+	require.Equal(t, user1.Email, user2.Email)
+	require.NotEmpty(t, user2.CreatedAt)
+}
+
+func TestGetUserById(t *testing.T) {
+	user1 := createRandomUser(t)
+	user2, err := testQueries.GetUserById(context.Background(), user1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user1.Username, user2.Username)
+	require.Equal(t, user1.Password, user2.Password)
+	require.Equal(t, user1.Email, user2.Email)
+	require.NotEmpty(t, user2.CreatedAt)
+}
