cd852d044e4d6235ace0fa944e56f3a402b74816
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-05-18 08:04:00 -0300

    feat: add random util

---
+++ util/random.go
@@ -0,0 +1,30 @@
+package util
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomString(number int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < number; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomEmail(number int) string {
+	return fmt.Sprintf("%s@email.com", RandomString(number))
+}
