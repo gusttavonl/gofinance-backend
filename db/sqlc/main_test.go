777169c0b479c727843b59d8b6e7f38bade1578c
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-05-18 08:05:23 -0300

    test: add test to main

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://postgres:postgres@localhost:5432/go_finance?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+	testQueries = New(conn)
+	os.Exit(m.Run())
+}
