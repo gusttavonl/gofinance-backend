28605b5e2cd401379d202caa7cb1399f0753f37f
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-05-19 08:34:50 -0300

    feat: add server api

---
+++ api/server.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	db "github.com/GustavoNoronha0/gofinance-backend/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.SQLStore
+	router *gin.Engine
+}
+
+func NewServer(store *db.SQLStore) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/user", server.createUser)
+	router.GET("/user/:username", server.getUser)
+	router.GET("/user/:id", server.getUserById)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"api has error:": err.Error()}
+}
