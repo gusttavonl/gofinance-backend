617968ae64ffdb746eedd69da0912f9020d2b396
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-05-19 08:36:01 -0300

    feat: add main to start server

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/GustavoNoronha0/gofinance-backend/api"
+	db "github.com/GustavoNoronha0/gofinance-backend/db/sqlc"
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://postgres:postgres@localhost:5432/go_finance?sslmode=disable"
+	serverAddress = "0.0.0.0:8000"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start api: ", err)
+	}
+}
