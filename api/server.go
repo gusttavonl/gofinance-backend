c4c76df4be6aa9f6d69e501dc25ecd5e445e9342
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-05-19 09:11:36 -0300

    refactor: change params in route

--- api/server.go
+++ api/server.go
@@ -16,7 +16,7 @@ func NewServer(store *db.SQLStore) *Server {
 
 	router.POST("/user", server.createUser)
 	router.GET("/user/:username", server.getUser)
-	router.GET("/user/:id", server.getUserById)
+	router.GET("/user/id/:id", server.getUserById)
 
 	server.router = router
 	return server
