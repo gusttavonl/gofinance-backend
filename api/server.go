ba5f657847f552118255e9da26d7eea312c6a50e
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-05-20 09:05:07 -0300

    feat: create routes to category and account

--- api/server.go
+++ api/server.go
@@ -18,6 +18,20 @@ func NewServer(store *db.SQLStore) *Server {
 	router.GET("/user/:username", server.getUser)
 	router.GET("/user/id/:id", server.getUserById)
 
+	router.POST("/category", server.createCategory)
+	router.GET("/category/id/:id", server.getCategory)
+	router.GET("/category", server.getCategories)
+	router.DELETE("/category/:id", server.deleteCategory)
+	router.PUT("/category/:id", server.updateCategory)
+
+	router.POST("/account", server.createAccount)
+	router.GET("/account/id/:id", server.getAccount)
+	router.GET("/account", server.getAccounts)
+	router.GET("/account/graph/:user_id/:type", server.getAccountGraph)
+	router.GET("/account/reports/:user_id/:type", server.getAccountReports)
+	router.DELETE("/account/:id", server.deleteAccount)
+	router.PUT("/account/:id", server.updateAccount)
+
 	server.router = router
 	return server
 }
