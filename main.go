ff9686851536f159b0fab1c9c1e947f89dbf2cf0
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-07-03 21:24:44 -0300

    feat: add godotenv in main and use .env to connect db and server address

--- main.go
+++ main.go
@@ -3,19 +3,23 @@ package main
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	"github.com/GustavoNoronha0/gofinance-backend/api"
 	db "github.com/GustavoNoronha0/gofinance-backend/db/sqlc"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://postgres:postgres@localhost:5432/go_finance?sslmode=disable"
-	serverAddress = "0.0.0.0:8000"
-)
-
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbSource := os.Getenv("DB_SOURCE")
+	serverAddress := os.Getenv("SERVER_ADDRESS")
 	conn, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
