a11b813b2d5d81abff5bf3a986c967cf27bbd71f
Cleyson Lima <cleysonph@gmail.com>
2022-10-02 02:32:06 -0300

    refactor: update getCategories query

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ createdb:
 	createdb --username=postgres --owner=postgres go_finance
 
 postgres:
-	docker run --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=postgres -d postgres:14-alpine 
+	docker run --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=postgres -d postgres:14-alpine
 
 migrateup:
 	migrate -path db/migration -database "postgresql://postgres:postgres@localhost:5432/go_finance?sslmode=disable" -verbose up
@@ -16,4 +16,7 @@ test:
 server:
 	go run main.go
 
-.PHONY: createdb postgres dropdb migrateup migrationdrop test server
+sqlc-gen:
+	docker run --rm -v $$(pwd):/src -w /src kjconroy/sqlc generate
+
+.PHONY: createdb postgres dropdb migrateup migrationdrop test server sqlc-gen
--- db/query/category.sql
+++ db/query/category.sql
@@ -13,21 +13,27 @@ SELECT * FROM categories
 WHERE id = $1 LIMIT 1;
 
 -- name: GetCategories :many
-SELECT * FROM categories 
-where user_id = $1 and type = $2 
-and title like $3 and description like $4;
+SELECT * FROM categories
+WHERE
+  user_id = $1
+AND
+  type = $2
+AND
+  title LIKE CONCAT('%', @title::text, '%')
+AND
+  description LIKE CONCAT('%', @description::text, '%');
 
 -- name: GetCategoriesByUserIdAndType :many
-SELECT * FROM categories 
+SELECT * FROM categories
 where user_id = $1 and type = $2;
 
 -- name: GetCategoriesByUserIdAndTypeAndTitle :many
-SELECT * FROM categories 
+SELECT * FROM categories
 where user_id = $1 and type = $2
 and title like $3;
 
 -- name: GetCategoriesByUserIdAndTypeAndDescription :many
-SELECT * FROM categories 
+SELECT * FROM categories
 where user_id = $1 and type = $2
 and description like $3;
 
