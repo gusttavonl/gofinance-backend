8599bedc79b8f1fcee7001a82495800cf64a66ad
@gustavo.lima <gustavo.lima@vitta.me>
2023-04-02 21:31:04 -0300

    feat: add cors config func

--- api/server.go
+++ api/server.go
@@ -3,7 +3,6 @@ package api
 import (
 	db "github.com/GustavoNoronha0/gofinance-backend/db/sqlc"
 	"github.com/gin-gonic/gin"
-	cors "github.com/rs/cors/wrapper/gin"
 )
 
 type Server struct {
@@ -11,10 +10,26 @@ type Server struct {
 	router *gin.Engine
 }
 
+func CORSConfig() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		context.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		context.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		context.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		context.Writer.Header().Set("Access-Control-Allow-Methods", "POST, DELETE, GET, PUT")
+
+		if context.Request.Method == "OPTIONS" {
+			context.AbortWithStatus(204)
+			return
+		}
+
+		context.Next()
+	}
+}
+
 func NewServer(store *db.SQLStore) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
-	router.Use(cors.Default())
+	router.Use(CORSConfig())
 
 	router.POST("/user", server.createUser)
 	router.GET("/user/:username", server.getUser)
